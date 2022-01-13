90fbcc51dbd47d8196fd31d5f07716b66917e666
Pavel Mikhaylov <mykhailovpm@gmil.com>
2022-01-13 23:35:01 +0200

    added project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import desktopGirlsPhoto from "./assets/img/desktop/girls_photo.png";
+import desktopLetter from "./assets/img/desktop/letter_big.jpg";
+import mobileInstagramSmall from "./assets/img/mobile/instagram_small.png";
+import desktopLogoBig from "./assets/img/desktop/logo_big.png";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+        <div className="container-big container-fluid">
+            <div className="row">
+                <div className="inner-wrapper">
+                    <div className="logo">
+                        <img
+                            src={desktopLogoBig}
+                            alt="dress in the city logo"
+                        />
+                        <div className="social-icons">
+                            <a href="https://instagram.com/dressandthecity.ua">
+                                <img
+                                    src={mobileInstagramSmall}
+                                    alt="instagram link"
+                                />
+                            </a>
+                        </div>
+                    </div>
+                    <div className="main-section-wrapper">
+                        <div className="letter">
+                            <img
+                                src={desktopLetter}
+                                alt="about us"
+                            />
+                        </div>
+                        <div className="photo">
+                            <img
+                                src={desktopGirlsPhoto}
+                                alt="girls"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {/* <!-- NOTE: container small --> */}
+        <div className="container-small container-fluid">
+            <div className="row">
+                <div className="inner-wrapper">
+                    <div className="logo">
+                        <img
+                            src={desktopLogoBig}
+                            alt="dress in the city"
+                        />
+                    </div>
+                    <div className="social-icons">
+                        <a href="https://instagram.com/dressandthecity.ua">
+                            <img
+                                src={mobileInstagramSmall}
+                                alt="instagram link"
+                            />
+                        </a>
+                    </div>
+                    <div className="letter">
+                        <img
+                            src={desktopLetter}
+                            alt="about us"
+                        />
+                    </div>
+                    <div className="photo">
+                        <img
+                            src={desktopGirlsPhoto}
+                            alt="girls"
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
   );
 }
